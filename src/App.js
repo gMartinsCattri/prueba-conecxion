88f5d44ef5e7f8b009c980a796a1754397051ac1
guimartinsalmeida <guimartinsalmeida@gmail.com>
2022-08-10 15:06:10 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 const App = () => {
+
+
+  
   const [name, setName] = useState([{}]);
 
   useEffect(() => {
@@ -10,17 +13,24 @@ const App = () => {
     .then(res => setName(res));
     console.log("name", name)
     
-  }, [name]); 
+  }, []); 
 
-  return <div className="users">
-  {name.map(user => (
-        <div key={user}>
-          <h2>
-            Name: {user.name} id: {user.id}
-          </h2>
-        </div>
-      ))}
-</div>;
+  return <><div className="users">
+    {name.map(user => (
+      <div key={user}>
+        <h2>
+          Name: {user.name} id: {user.id}
+        </h2>
+      </div>
+    ))}
+  </div>
+  <div>
+      <video width="320" height="240" autoPlay controls muted >
+        <source id="music" src="http://74.208.169.34/videos/MexiquenseTV.mp4" type="video/mp4"></source>
+          Your browser does not support the video tag.
+        </video>
+    </div>
+    </>;
 }
 
 export default App;
